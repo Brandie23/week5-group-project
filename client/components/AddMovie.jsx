c589944caf1d9d788d84503c4fe9e3db558a525e
oliver-harcourt <oliver.harcourt@gmail.com>
2018-11-16 13:43:02 +1300

    added AddMovie component

---
+++ client/components/AddMovie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+class AddMovie extends React.Component {
+  render(props) {
+    return (
+      <p></p>
+    )
+  }
+}
+
+export default AddMovie
